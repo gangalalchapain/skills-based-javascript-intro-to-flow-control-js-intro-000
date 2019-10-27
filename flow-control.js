2f948867768a305f0fb6c5cfd9720edac20b0b03
Ganga Lal Chapain <gangalalchapain@gmail.com>
2019-10-27 13:21:19 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,10 +1,11 @@
 function basicTeenager(age) {
+  var result="";
   if (age >=13 && age <=19){
-    console.log("You are a teenager!");
+    result = "You are a teenager!";
   }else{
-    console.log("You are not a teenager")
+  result = "You are not a teenager";
   }
-
+  return result;
 }
 
 function teenager(age) {
